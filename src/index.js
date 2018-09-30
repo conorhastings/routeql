4f78b07e14587223e2f1da2b04198994b4d2844a
Conor Hastings <hastings.conorm@gmail.com>
2018-09-29 20:48:06 -0700

    export parseQuery:

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+import { parse } from "graphql/language/parser";
 import routeql from './routeql';
 import Query from './Query';
 
 export default routeql;
-export { routeql, Query };
+export { routeql, Query, parse as parseQuery };
